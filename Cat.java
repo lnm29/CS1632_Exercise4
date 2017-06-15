d4bf8642d7beb44b26c47e25e205343da6bd7be9
lnm29 <lnm29@pitt.edu>
2017-06-15 12:51:20 -0400

    Create Cat.java
    This is what's here so far, feel free to change

---
+++ Cat.java
@@ -0,0 +1,19 @@
+public class Cat {
+
+		
+	public Cat(){}
+	
+	
+
+	public String getCatName(){
+		return this.catName;
+	}
+	
+	public int getCatID(){
+		return this.catID;
+	}
+	
+	public int getCatPrice(){
+		return this.catPrice;
+	}
+}
