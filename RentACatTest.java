f35cb05da9329104ce5386a71ec41da35dffa0ee
lnm29 <lnm29@pitt.edu>
2017-06-15 13:52:59 -0400

    Update RentACatTest.java

--- RentACatTest.java
+++ RentACatTest.java
@@ -1,1 +1,38 @@
 
+public class RentACatTest {
+	
+	Cat cat = null;
+	
+	//Create an instance of Cat before it exists
+	@Before
+	public void init(){
+		cat = new Cat();
+	}
+	
+	//Two tests for rentCat method
+	@Test
+	public void catExist(){
+		assertNotNull(cat);
+	}
+	
+	@Test
+	public void catNotExist(){
+		assertNull(cat);
+	}
+	
+	//Two methods for returnCat method
+	@Test
+	public void catReturned(){
+		
+	}
+	
+	@Test
+	public void catRented(){
+		
+	}
+	
+	
+	
+	
+	
+}
