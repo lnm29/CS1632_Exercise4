a890f076485b10658b3ddb3fa4e910b6e4f4646d
Furst <AJF80@pitt.edu>
2017-06-15 13:15:40 -0400

    classes added

--- Cat.java
+++ Cat.java
@@ -1,19 +1,38 @@
-public class Cat {
-
-		
-	public Cat(){}
+public class Cat{
 	
+	private final String name;
+	private final double cost;
+	private Customer renter;
+	private final int ID;
+	private boolean isRented;
 	
-
-	public String getCatName(){
-		return this.catName;
+	Cat(String newName, double newCost, int newID){
+		this.name = newName;
+		this.cost = newCost;
+		this.renter = null;
+		this.ID = newID;
+		this.isRented = false;
 	}
 	
-	public int getCatID(){
-		return this.catID;
+	public void rentCat(Customer newRenter){
+		if(this.isRented == true){
+			System.out.println("Sorry, " + this.name + " is not here!");
+		}else{
+			System.out.println(this.name + " has been rented to Customer " + newRenter.name + ".");
+			this.isRented = true;
+			this.renter = newRenter;
+		}
 	}
 	
-	public int getCatPrice(){
-		return this.catPrice;
+	public void returnCat(){
+		System.out.println(this.renter + " paid " + this.cost + ".");
+		this.renter = null;
+		this.isRented = false;
+	}
+	
+	//ID 3. Mistoffelees: $500.00 / day
+	public String toString(){
+		String output = "ID " + this.ID + ". " + this.name + ": $" + this.cost + " / day";
+		return output;
 	}
 }
---
+++ Customer.java
@@ -0,0 +1,9 @@
+public class Customer{
+	String name;
+	int ID;
+	
+	Customer(String newName, int newID){
+		this.name = newName;
+		this.ID = newID;
+	}
+}
